ccb8b379f05e5165a174cb57925873dec7de6d9f
Hamza Khan <56268145+Hkhan221@users.noreply.github.com>
2023-01-14 16:05:05 +0000

    Navbar: added navbar draft

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>HACK INDUSTRIES</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About us</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="shop">Shop</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="sign up">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='signin'>Sign in</NavBtnLink>
+          </NavBtn>    
+        </NavbarContainer>
+      </Nav>
+    </div>
+  );
+};
+
+export default Navbar;
